e06ec70da0ede05aeed5db4db15fa6e81fe596c4
Josh Bernstein <joshua.bernstein@outlook.com>
2021-09-22 16:58:39 -0600

    Added urls for github and internal links

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -20,8 +20,9 @@ export enum TechTypes {
 export interface IProject {
   name: string;
   description: string;
-  url: string;
-  external_url: string;
+  internal_url?: string;
+  github_url?: string;
+  youtube_url?: string;
   stack: TechTypes[];
 }
 
@@ -38,8 +39,8 @@ const Projects: NextPage = () => {
         name: 'Project Notebook',
         description:
           'Project Notebook is an integrated note taking + task management system with out of the box dashboards for each project to track progress',
-        url: 'https://github.com/codefordenver/energy-assistance-dashboard',
-        external_url: 'https://www.youtube.com',
+        github_url:
+          'https://github.com/codefordenver/energy-assistance-dashboard',
         stack: [
           TechTypes.React,
           TechTypes.NEXTJS,
@@ -53,8 +54,9 @@ const Projects: NextPage = () => {
         name: 'Energy Assistance Dashboard',
         description:
           'This dashboard was built for Energy Outreach Colorado in order to visualize the energy assistance requirements of various colorado counties.',
-        url: 'https://github.com/codefordenver/energy-assistance-dashboard',
-        external_url: 'https://www.youtube.com',
+        github_url:
+          'https://github.com/codefordenver/energy-assistance-dashboard',
+        youtube_url: 'https://www.youtube.com',
         stack: [
           TechTypes.React,
           TechTypes.NEXTJS,
@@ -67,24 +69,24 @@ const Projects: NextPage = () => {
         name: 'Community Radio Scheduler (Comrad)',
         description:
           'Comrad is an open-source web application for use by community radio stations that helps with crucial show scheduling and playlist entry to organize all on air and streaming processes for hosts and DJ’s.',
-        url: 'https://github.com/codefordenver/Comrad',
-        external_url: 'https://www.youtube.com',
+        github_url: 'https://github.com/codefordenver/Comrad',
+        youtube_url: 'https://www.youtube.com',
         stack: [TechTypes.React, TechTypes.NodeJS],
       },
       {
         name: 'ideaLab ',
         description:
           'ideaLab is a queuing app for 3D printing. It includes a user-facing form that allows the user to request a print job. It also consists of an employee-facing app that will enable them to change the statuses of print jobs, view print jobs, and modify print jobs. The web app is frontend React and backend Java Spring.',
-        url: 'https://github.com/codefordenver/ideaLab',
-        external_url: 'https://www.youtube.com',
+        github_url: 'https://github.com/codefordenver/ideaLab',
+        youtube_url: 'https://www.youtube.com',
         stack: [TechTypes.React, TechTypes.Spring],
       },
       {
         name: 'Habitat Group Contacts',
         description:
           'The Habitat for Humanity group contacts project is a URL shortening service that connects with their VolunteerHub scheduling system.  It allows their admin to send out up-to-date contact information to their group leaders with a sharable link that will query VolunteerHub on the backend.',
-        url: 'https://github.com/codefordenver/habitat-group-contacts',
-        external_url: 'https://www.youtube.com',
+        github_url: 'https://github.com/codefordenver/habitat-group-contacts',
+        youtube_url: 'https://www.youtube.com',
         stack: [TechTypes.React, TechTypes.NodeJS],
       },
     ],
@@ -93,120 +95,120 @@ const Projects: NextPage = () => {
         name: 'Blurry Background',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        url: 'blurry-backgrounds',
-        external_url: 'https://www.youtube.com',
+        internal_url: '/tailwindcss/blurry-backgrounds',
+        youtube_url: 'https://www.youtube.com',
         stack: [TechTypes.HTML, TechTypes.TailwindCSS],
       },
       {
         name: 'Responsive Tile Grid',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        url: 'responsive-tile-grid',
-        external_url: 'https://www.youtube.com',
+        internal_url: '/tailwindcss/responsive-tile-grid',
+        youtube_url: 'https://www.youtube.com',
         stack: [TechTypes.HTML, TechTypes.TailwindCSS],
       },
       {
         name: 'Box Decoration',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        url: 'box-decoration',
-        external_url: 'https://www.youtube.com',
+        internal_url: '/tailwindcss/box-decoration',
+        youtube_url: 'https://www.youtube.com',
         stack: [TechTypes.HTML, TechTypes.TailwindCSS],
       },
       {
         name: 'Background Blending',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        url: 'background-blending',
-        external_url: 'https://www.youtube.com',
+        internal_url: '/tailwindcss/background-blending',
+        youtube_url: 'https://www.youtube.com',
         stack: [TechTypes.HTML, TechTypes.TailwindCSS],
       },
       {
         name: 'Background Gradient',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        url: 'background-gradient',
-        external_url: 'https://www.youtube.com',
+        internal_url: '/tailwindcss/background-gradient',
+        youtube_url: 'https://www.youtube.com',
         stack: [TechTypes.HTML, TechTypes.TailwindCSS],
       },
       {
         name: 'iPhone',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        url: 'iphone',
-        external_url: 'https://www.youtube.com',
+        internal_url: '/tailwindcss/iphone',
+        youtube_url: 'https://www.youtube.com',
         stack: [TechTypes.HTML, TechTypes.TailwindCSS],
       },
       {
         name: 'Slick Fits',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        url: 'slick-fits',
-        external_url: 'https://www.youtube.com',
+        internal_url: '/tailwindcss/slick-fits',
+        youtube_url: 'https://www.youtube.com',
         stack: [TechTypes.HTML, TechTypes.TailwindCSS],
       },
       {
         name: 'Form',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        url: 'form',
-        external_url: 'https://www.youtube.com',
+        internal_url: '/tailwindcss/form',
+        youtube_url: 'https://www.youtube.com',
         stack: [TechTypes.HTML, TechTypes.TailwindCSS],
       },
       {
         name: 'Dropdown',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        url: 'dropdown',
-        external_url: 'https://www.youtube.com',
+        internal_url: '/tailwindcss/dropdown',
+        youtube_url: 'https://www.youtube.com',
         stack: [TechTypes.HTML, TechTypes.TailwindCSS],
       },
       {
         name: 'Switch',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        url: 'switch',
-        external_url: 'https://www.youtube.com',
+        internal_url: '/tailwindcss/switch',
+        youtube_url: 'https://www.youtube.com',
         stack: [TechTypes.HTML, TechTypes.TailwindCSS],
       },
       {
         name: 'Labels',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        url: 'floating-labels',
-        external_url: 'https://www.youtube.com',
+        internal_url: '/tailwindcss/floating-labels',
+        youtube_url: 'https://www.youtube.com',
         stack: [TechTypes.HTML, TechTypes.TailwindCSS],
       },
       {
         name: 'Stacked Layouts',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        url: 'stacked',
-        external_url: 'https://www.youtube.com',
+        internal_url: '/tailwindcss/stacked',
+        youtube_url: 'https://www.youtube.com',
         stack: [TechTypes.HTML, TechTypes.TailwindCSS],
       },
       {
         name: 'Stories',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        url: 'stories',
-        external_url: 'https://www.youtube.com',
+        internal_url: '/tailwindcss/stories',
+        youtube_url: 'https://www.youtube.com',
         stack: [TechTypes.HTML, TechTypes.TailwindCSS],
       },
       {
         name: 'Backdrop Filters',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        url: 'backdrop-filters',
-        external_url: 'https://www.youtube.com',
+        internal_url: '/tailwindcss/backdrop-filters',
+        youtube_url: 'https://www.youtube.com',
         stack: [TechTypes.HTML, TechTypes.TailwindCSS],
       },
       {
         name: 'Theme',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        url: 'theme',
-        external_url: 'https://www.youtube.com',
+        internal_url: '/tailwindcss/theme',
+        youtube_url: 'https://www.youtube.com',
         stack: [TechTypes.HTML, TechTypes.TailwindCSS],
       },
     ],
